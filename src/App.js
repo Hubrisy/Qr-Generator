c2d1d9813eab6b40136f0f9eed81ccd06b72f521
Your Name <Hubrisy>
2023-06-30 12:50:53 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import QRCode from "qrcode.react";
+import { useState } from 'react';
+import {jsPDF} from 'jspdf'
 
 function App() {
+
+  const [isQr, setIsQr] = useState('');
+
+  const generatePDF = () => {
+
+    // Defines the pdf
+    let pdf = new jsPDF({
+        orientation: 'landscape',
+        unit: 'mm',
+        format: [40, 40]
+    })
+
+    // Transforms the canvas into a base64 image
+    let base64Image = document.getElementById('qrcode').toDataURL()
+
+    // Adds the image to the pdf
+    pdf.addImage(base64Image, 'png', 0, 0, 40, 40)
+
+    // Downloads the pdf
+    pdf.save('QR.pdf')
+
+}
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main__container'>
+        <h1>QR Generator</h1>
+        <input onChange={(e) => setIsQr(e.target.value)} placeholder='type your url here...' />
+        <QRCode className='qr' value={isQr} id='qrcode' />
+        <button onClick={() => generatePDF()}>Download</button>
+      </div>
     </div>
   );
 }
